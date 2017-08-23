71a25860dbddb64ff7edac32e35761bbf14bb1d8
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-23 11:37:46 +0100

    Updated Gemfile, rspec and simplecov init

--- Gemfile
+++ Gemfile
@@ -5,3 +5,14 @@ ruby '2.4.0'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'rake'
+gem 'sinatra'
+
+group :test do
+  gem 'capybara'
+  gem 'launchy'
+  gem 'rspec'
+  gem 'rubocop'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '../app/rps_web.rb')
+Capybara.app = RPSWeb
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end

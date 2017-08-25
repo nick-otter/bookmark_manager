4f11a5a6f9b5248cc38ee08c3a2678a2b735321c
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-25 12:44:10 +0100

    Updating heroku enviroment

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,19 @@ require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  configure :development do
+    enable :logging, :dump_errors, :raise_errors
+    disable :show_exceptions
+    DataMapper::Logger.new(STDOUT, :debug, '[DataMapper] ')
+    DataMapper::Model.raise_on_save_failure = true
+  end
+
+  configure :test do
+    enable :dump_errors, :raise_errors
+    disable :run, :logging, :show_exceptions
+  end
+
   get '/links' do
     @links = Link.all
     erb :'/links/index'
--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,6 @@ property :url,    String
 
 end
 
-DataMapper.setup(:default, ENV['bookmark_manager_development']"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
